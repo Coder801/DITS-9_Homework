ea825810450c1e4ccecb7b6e461f24bd375d7047
vy4eslav.tka4enko <vya4eslav.tka4enko@gmail.com>
2019-02-13 12:20:20 +0200

    tkachenko/conditions_loops

---
+++ vtkachenko/04 - Conditionals,Loops/Finish/js/script.js
@@ -0,0 +1,196 @@
+console.clear();
+
+// ## 0 Проверка диапазона
+//
+// Напишите код который б проверял длинну телефонного номера
+// Номер запрашивается из prompt
+// Сообшение выводится через alert
+// Длинна номера не должна быть не меньше 9 и не больше 11
+
+// * Your code Start *
+let phoneNumber = prompt('Enter your phone number:');
+let phoneCheck = function(phoneNumber){
+  if(phoneNumber.length < 9 || phoneNumber.length > 11 ){
+    alert('Phone < 9 and >11');
+  }
+};
+phoneCheck(phoneNumber);
+// * Your code End *
+// ## 1 Клонирование обьектов
+//
+// Склонируйте обьект objectA в objectB при помощи цикла
+// Не забывайте что простое присвоение не клонирует обьект, вам нужно создать независимый клон
+
+const objectA = {
+  name: 'John',
+  lastname: 'Dou',
+  age: 50
+}
+
+const objectB = {};
+// * Your code Start *
+let copyObject = function(objectA,objectB){
+  for(let item in objectA){
+    objectB[item] = objectA[item];
+  }
+};
+copyObject(objectA,objectB);
+console.log(objectB);
+// * Your code End *
+
+// Переопределяем значения обьекта objectA
+objectA.name = 'Chris';
+objectA.name = 'Couer';
+objectA.name = '45';
+
+// Если обьект objectB склонирован, значения в нем меняться не будут
+// Выводим значения склонированного обьекта для проверки
+console.log(objectB.name); // John
+console.log(objectB.lastname); // Dou
+console.log(objectB.age); // 50
+// ## 2 Ветвление
+// При помощи конструкци switch/case выведете текущую дату
+// Название месяца должно иметь вид строки
+// Например = 14 Июня 2017
+
+const date = new Date();
+let day = date.getDate(); // Текущий день
+let month = date.getMonth(); // Текущий месяц (от 0 до 11)
+let year = date.getFullYear(); // Текущий год
+let monthName = ''; // Переменная для значения месяца
+
+// * Your code Start *
+switch(month){
+  case 0:monthName = 'Января';
+  break;
+  case 1:monthName = 'Февраля';
+  break;
+  case 2:monthName = 'Марта';
+  break;
+  case 3:monthName = 'Апреля';
+  break;
+  case 4:monthName = 'Мая';
+  break;
+  case 5:monthName = 'Июня';
+  break;
+  case 6:monthName = 'Июля';
+  break;
+  case 7:monthName = 'Августа';
+  break;
+  case 8:monthName = 'Сеньтября';
+  break;
+  case 9:monthName = 'Октября';
+  break;
+  case 10:monthName = 'Ноября';
+  break;
+  case 11:monthName = 'Декабря';
+  break;
+}
+// * Your code End *
+
+console.log(`Today is: ${day} ${monthName} ${year}`) // Пример: 10 Февраля 2019
+// ## 3 Взаимодействие с пользователем
+//
+// Запросите через prompt у пользователя его имя;
+// Запрашивайте имя пользователя, пока одно из имен не будет совпадать с одним из массива users
+// После того как пользователь введет имя которое есть в обьекте users, запросите у него пароль
+// -- Если пароль введен не верно выведете alert об этом и вернитесь на повторый ввод имени
+// -- Если пароль совпадает - вывести сообщение о том что пароль верный и прервите цикл
+
+const users = [{
+  name: 'Alex',
+  password: 123
+}, {
+  name: 'Chris',
+  password: 456
+}, {
+  name: 'Bill',
+  password: 789
+}];
+
+// * Your code Start *
+let checkUser = function(users){
+  let userName = prompt('Enter user name');
+  for(var key in users){
+    (userName == users[key].name) ? checkPass(users[key]) : checkUser(users);
+  }
+}
+let checkPass = function(user){
+  let userPass = prompt('Enter password ');
+  if(userPass == user.password) {
+    alert('You enter');exit;
+  }else{
+    checkUser();
+  } 
+}
+checkUser(users);
+// * Your code End *
+
+
+// ## 4
+// Создайте беспрерывный цикл который принимает от пользователя числа, и суммирует
+// Цикл прирывается только в том случае если пользователь ввел что либо кроме числа
+// После прерывания цикла выводится сумма введеных чисел
+
+//const sumArray = prompt('Please enter number', 0);
+  
+// * Your code Start *
+ let sum = 0;
+ do{
+   let enterNumber = +prompt('Enter the number','');
+   if(!enterNumber) break;
+   sum+=enterNumber;
+ }while(true);
+ alert(sum);
+
+// * Your code End *
+
+// ## 5
+// Найдите сумму  1 + 2 + 3 + ... + N, где число N вводится пользователем из prompt
+// Результат вывести в alert
+
+const summetNumber = Number(prompt('Please enter number', 0));
+
+// * Your code Start *
+let numberSum = function(summetNumber){
+  return(summetNumber != 1) ? summetNumber + numberSum(summetNumber-1) : 1;
+};
+console.log(numberSum(summetNumber));
+// * Your code End *
+
+
+// ## 6
+// Нарисовать шахматную доску используя вложенные циклы (Доска находится в файле html)
+// Размер шахматной доски стандартны 8х8
+// Функция drawFractions - вспомогательная, при ее вызове в к блоку board в html добаляется 1 квардрат доски
+// Ее можно не трогать
+// Например что бы нарисовать 1 квадрат нужно вызвать ее 1 раз: drawFraction();
+// Так же доски должны иметь черный\белый цвет. Реализовать это можно при помощи как css так и js
+
+// const drawFraction = function(){
+//   const board = document.querySelector('.board');
+//   const fraction = document.createElement('div');
+//   board.appendChild(fraction);
+// }
+
+// const size = 8;
+
+// * Your code Start *
+const drawFraction = function(){
+  const board = document.querySelector('.board');
+  let fraction;
+  let switchBlackWhite = true;
+  const size = 8;
+  for(let i = 0;i < size;i++){
+    for(let j = 0;j < size;j++){
+      if(j==0) switchBlackWhite = !switchBlackWhite; 
+      fraction = document.createElement('div');
+      switchBlackWhite ? fraction.className = 'black':fraction.className = 'white';
+      board.appendChild(fraction);
+      switchBlackWhite = !switchBlackWhite;
+    }
+  }
+};
+drawFraction();
+
+// * Your code End *
